2408dc80464cd21be05f77c7ca54f5c2421c038e
unknown <aliahmad538404@gmail.com>
2021-02-21 15:08:51 +0500

    Alter 2 with gunicorn and procfile

--- ECM/settings.py
+++ ECM/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'o83$wms9e4u&v)8vrjq3seeh1o1v+lufd!p)sc)+x(o^g2d(#5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
