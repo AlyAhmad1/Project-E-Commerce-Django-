2e4c3c59d04b36ba4ff7674bb4413ab0533e00bf
unknown <aliahmad538404@gmail.com>
2021-02-28 20:20:10 +0500

    Upadate static path

--- ECM/settings.py
+++ ECM/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
