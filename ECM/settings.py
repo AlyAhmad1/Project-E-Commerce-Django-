dba06ef9875ce62b6b8839e2fd3e3d4d35ec57a3
unknown <aliahmad538404@gmail.com>
2021-03-03 01:39:55 +0500

    Updated with Postgres Done13

--- ECM/settings.py
+++ ECM/settings.py
@@ -137,8 +137,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
