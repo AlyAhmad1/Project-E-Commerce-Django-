a5c3df5766c1bc1fda422934f794c8fc0b00d544
unknown <aliahmad538404@gmail.com>
2021-03-03 01:33:37 +0500

    Updated with Postgres Done12

--- ECM/settings.py
+++ ECM/settings.py
@@ -133,16 +133,17 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
-# STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "staticfiles"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR,"static")
 MEDIA_ROOT = os.path.join(BASE_DIR,"live-static", "media-root")
 
 
