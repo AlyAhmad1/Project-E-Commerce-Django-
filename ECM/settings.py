173c69abb0a9b173905a9361368a2061d6d45f9c
unknown <aliahmad538404@gmail.com>
2021-03-03 01:59:01 +0500

    Updated with Postgres Done15

--- ECM/settings.py
+++ ECM/settings.py
@@ -131,17 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
-# STATIC_ROOT = os.path.join(BASE_DIR,"static")
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR,"live-static", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 django_heroku.settings(locals())
