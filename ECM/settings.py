2045072dd70687d9b35b7ead1d08ed704c35d36f
unknown <aliahmad538404@gmail.com>
2021-03-03 02:41:10 +0500

    Updated with Postgres Done22

--- ECM/settings.py
+++ ECM/settings.py
@@ -133,14 +133,15 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, "new-static","static-root")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "new-static", "media-root")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
